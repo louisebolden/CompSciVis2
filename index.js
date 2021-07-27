87fb4fa17e0e4ee8e202742c351b5083f035c29a
Louise Swift <louise@louiseswift.com>
2021-07-27 09:19:38 +0100

    wip

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const timeline = anime.timeline({
-  begin: () => console.log("beginning"),
-  complete: () => console.log("complete"),
-  easing: 'linear',
-  targets: '.matrix',
-});
+// const timeline = anime.timeline({
+//   begin: () => console.log("beginning"),
+//   complete: () => console.log("complete"),
+//   duration: 350,
+//   easing: 'linear',
+//   targets: '.matrix',
+// });
 
-timeline.add({
-    opacity: 1,
-    // strokeDashoffset: [anime.setDashoffset, 0],
-    // duration: 350,
-    // delay: anime.stagger(350),
-})
+// timeline.add({
+//     // opacity: 1,
+//     // strokeDashoffset: [anime.setDashoffset, 0],
+//     // duration: 350,
+//     // delay: anime.stagger(350),
+// })
+
+window.addEventListener("load", function() {
+  const targets = document.querySelectorAll(".matrix .element .inner");
+  console.log("> targets:", targets);
+
+  const animation = anime.timeline({
+    begin: () => console.log("beginning"),
+    complete: () => console.log("complete"),
+    // delay: anime.stagger(100, {
+    //   grid: [3, 3],
+    //   from: "center"
+    // }),
+    delay: anime.stagger(100),
+    // loop: true,
+    targets: targets,
+  });
+
+  animation
+    .add({
+      // backgroundColor: 'purple',
+      // scale: 0.5,
+      translateY: '-10px'
+    })
+    .add({
+      // backgroundColor: 'orange',
+      // scale: 1,
+      translateY: 0
+    })
+});
